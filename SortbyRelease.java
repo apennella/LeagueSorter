23926a81cebf873cf0b07904beaeee337fc38859
apennella <adpennella@gmail.com>
2019-08-28 11:42:58 -0400

    Update SortbyRelease.java

--- SortbyRelease.java
+++ SortbyRelease.java
@@ -8,7 +8,7 @@ public class SortbyRelease  implements Comparator<Champion>{
 
 	@Override
 	public int compare(Champion o1, Champion o2) {
-		// TODO Auto-generated method stub
+		
 		return o1.getRelease().compareTo(o2.getRelease());
 	}
 
